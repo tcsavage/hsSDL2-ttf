38ce1be2f9d88e75b2fab782b62ae9fd7ac35462
tcsavage <tcsavage@gmail.com>
2013-09-22 21:31:11 +0100

    Change #import to #include

--- cbits/wrappers.c
+++ cbits/wrappers.c
@@ -1,6 +1,6 @@
-#import <SDL2/SDL.h>
-#import <SDL2/SDL_ttf.h>
-#import "wrappers.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include "wrappers.h"
 
 SDL_Surface *TTF_RenderText_Solid_Ptrd(TTF_Font *font, const char *text, SDL_Color *fg) {
     return TTF_RenderText_Solid(font, text, *fg);
--- cbits/wrappers.h
+++ cbits/wrappers.h
@@ -1,4 +1,4 @@
-#import <SDL2/SDL.h>
-#import <SDL2/SDL_ttf.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 SDL_Surface *TTF_RenderText_Solid_Ptrd(TTF_Font *font, const char *text, SDL_Color *fg);
